4d4599facca6b29d0cfa94de0f06531a1fff3e42
Yutaka Kubota <jaz@kubothink.com>
2020-07-25 22:42:45 +0900

    Create Xcode project.

---
+++ SurveyKit/SurveyKit.h
@@ -0,0 +1,19 @@
+//
+//  SurveyKit.h
+//  SurveyKit
+//
+//  Created by Yutaka Kubota on 2020/07/25.
+//  Copyright © 2020 Yui-Wroks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SurveyKit.
+FOUNDATION_EXPORT double SurveyKitVersionNumber;
+
+//! Project version string for SurveyKit.
+FOUNDATION_EXPORT const unsigned char SurveyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurveyKit/PublicHeader.h>
+
+
